f027174a6123bbcbc8eea3de137740653865aabd
Paolo <elementale63@hotmail.com>
2011-04-06 11:35:44 +0200

    mod hook

--- lib/linked_fields_hooks.rb
+++ lib/linked_fields_hooks.rb
@@ -3,12 +3,11 @@ class LinkedFieldsHooks < Redmine::Hook::ViewListener
  def view_issues_form_details_bottom(params)
         html = ""
         html += "<p>"
-        html += params[:form].select :id, TypeCustomFields.find(:all, :conditions => {:riferimento => "issue"}).collect{ |t| [t.descrizione, t.id]}, :include_blank => true
-        html += params[:form].select :id, LinkedCustomFields.find(:all, :conditions => {:tipologia => type.id}).collect{ |l| [l.valore, l.id]}, :include_blank => true
+        html += params[:form].select :type_id, TypeCustomFields.find(:all, :conditions => {:riferimento => "issue"}).collect{ |t| [t.descrizione, t.id]}, :include_blank => true
+        html += "&nbsp;&nbsp;"
+        html += params[:form].select :linked_custom_id, LinkedCustomFields.find(:all, :conditions => {:tipologia => :type_id}).collect{ |l| [l.valore, l.id]}, :include_blank => true
         html += "</p>"
         html
-        # user = params[:user]
-        # "ciao"
     end
     
 end
