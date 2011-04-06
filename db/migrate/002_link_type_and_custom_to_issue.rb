55738d0d731de3edf3d8199664ae457939825e56
Paolo <elementale63@hotmail.com>
2011-04-06 12:00:55 +0200

    impaginazione

---
+++ db/migrate/002_link_type_and_custom_to_issue.rb
@@ -0,0 +1,12 @@
+# Use rake db:migrate_plugins to migrate installed plugins
+class LinkTypeAndCustomToIssue < ActiveRecord::Migration
+  def self.up
+    add_column :issues, :type_id, :integer
+    add_column :issues, :linked_custom_id, :integer   
+  end
+
+  def self.down
+    remove_column :users, :type_id
+    remove_column :users, :linked_custom_id
+  end
+end
--- lib/linked_fields_hooks.rb
+++ lib/linked_fields_hooks.rb
@@ -4,10 +4,12 @@ class LinkedFieldsHooks < Redmine::Hook::ViewListener
         html = ""
         html += "<p>"
         html += params[:form].select :type_id, TypeCustomFields.find(:all, :conditions => {:riferimento => "issue"}).collect{ |t| [t.descrizione, t.id]}, :include_blank => true
-        html += "&nbsp;&nbsp;"
+        html += "</p>"
+        html += "<p>"
         html += params[:form].select :linked_custom_id, LinkedCustomFields.find(:all, :conditions => {:tipologia => :type_id}).collect{ |l| [l.valore, l.id]}, :include_blank => true
         html += "</p>"
         html
+       
     end
     
 end
