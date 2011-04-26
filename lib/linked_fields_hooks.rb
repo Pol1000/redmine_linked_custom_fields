b8f9b60291df795e919d6f66b1e17568dfddf010
Paolo <elementale63@hotmail.com>
2011-04-26 17:18:27 +0200

    aggiunta gestione permessi

--- lib/linked_fields_hooks.rb
+++ lib/linked_fields_hooks.rb
@@ -2,6 +2,7 @@ class LinkedFieldsHooks < Redmine::Hook::ViewListener
  include ActionView::Helpers::PrototypeHelper
  
  def view_issues_form_details_bottom(params)
+    if authorize_for('linked_custom_fields', 'gestione')
         html = ""
         html += "<p>"
         html += params[:form].select :linked_custom_id1, LinkedCustomFields.find_by_sql("select linked_custom_fields.* from linked_custom_fields inner join type_custom_fields on linked_custom_fields.tipologia = type_custom_fields.id where (type_custom_fields.figlia_di IS NULL) AND (type_custom_fields.riferimento ='issue') ").collect{ |t| [t.valore, t.id]}, :include_blank => true
@@ -11,7 +12,7 @@ class LinkedFieldsHooks < Redmine::Hook::ViewListener
         html += "<div id='primo_select'></div>"
         html += "</p>"
         html
-       
+    end   
     end
     # select :linked_custom_id, LinkedCustomFields.find(:all, :conditions => {:tipologia => :type_id}).collect{ |l| [l.valore, l.id]}, :include_blank => true
  def protect_against_forgery?
