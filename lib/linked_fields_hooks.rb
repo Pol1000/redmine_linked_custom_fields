8732ca3e7524b63f87a15ccfa63da6411f9401dc
Paolo <elementale63@hotmail.com>
2011-04-27 09:57:43 +0200

    aggiunta gestione permessi(finito)

--- lib/linked_fields_hooks.rb
+++ lib/linked_fields_hooks.rb
@@ -1,9 +1,12 @@
 class LinkedFieldsHooks < Redmine::Hook::ViewListener
+ 
  include ActionView::Helpers::PrototypeHelper
  
  def view_issues_form_details_bottom(params)
-    if authorize_for('linked_custom_fields', 'gestione')
-        html = ""
+   
+       @issue = params[:issue]
+    if @issue.project.module_enabled?(:linked_custom_fields)
+        html =""
         html += "<p>"
         html += params[:form].select :linked_custom_id1, LinkedCustomFields.find_by_sql("select linked_custom_fields.* from linked_custom_fields inner join type_custom_fields on linked_custom_fields.tipologia = type_custom_fields.id where (type_custom_fields.figlia_di IS NULL) AND (type_custom_fields.riferimento ='issue') ").collect{ |t| [t.valore, t.id]}, :include_blank => true
         html += observe_field :issue_linked_custom_id1, :url => {:controller => 'linked_custom_fields', :action => :update_form_issue1},
@@ -12,10 +15,13 @@ class LinkedFieldsHooks < Redmine::Hook::ViewListener
         html += "<div id='primo_select'></div>"
         html += "</p>"
         html
-    end   
+   end    
     end
-    # select :linked_custom_id, LinkedCustomFields.find(:all, :conditions => {:tipologia => :type_id}).collect{ |l| [l.valore, l.id]}, :include_blank => true
+
  def protect_against_forgery?
     false
    end
 end
+
+
+#    if authorize_for('linked_custom_fields', 'create')
