9b3362719ee169582e5bd7f47daf5fbcc036b95e
dk201100022 <elementale63@hotmail.com>
2011-11-16 16:10:25 +0100

    modify the issue list user can see

--- lib/linked_fields_hooks.rb
+++ lib/linked_fields_hooks.rb
@@ -8,11 +8,11 @@ class LinkedFieldsHooks < Redmine::Hook::ViewListener
     if @issue.project.module_enabled?(:linked_custom_fields)
         html =""
         html += "<p>"
-        html += params[:form].select :linked_custom_id1, LinkedCustomFields.find_by_sql("select linked_custom_fields.* from linked_custom_fields inner join type_custom_fields on linked_custom_fields.tipologia = type_custom_fields.id where (type_custom_fields.figlia_di IS NULL) AND (type_custom_fields.riferimento ='issue') ").collect{ |t| [t.valore, t.id]}, :include_blank => true
+        html += params[:form].select :linked_custom_id1, LinkedCustomFields.find_by_sql("select linked_custom_fields.* from linked_custom_fields inner join type_custom_fields on linked_custom_fields.tipologia = type_custom_fields.id where (type_custom_fields.figlia_di IS NULL) AND (type_custom_fields.riferimento ='issue') ").collect{ |t| [t.valore, t.id]},:required => true, :include_blank => true
         html += "</p>"
         
         html += "<p>"
-        html += params[:form].select :linked_custom_id2,LinkedCustomFields.find_by_sql("select linked_custom_fields.* from linked_custom_fields inner join type_custom_fields on linked_custom_fields.tipologia = type_custom_fields.id where (type_custom_fields.figlia_di in (select id from type_custom_fields where (type_custom_fields.figlia_di IS NULL) AND (type_custom_fields.riferimento ='issue')) AND (type_custom_fields.riferimento ='issue') )").collect{ |t| [t.valore, t.id]}, :include_blank => true
+        html += params[:form].select :linked_custom_id2,LinkedCustomFields.find_by_sql("select linked_custom_fields.* from linked_custom_fields inner join type_custom_fields on linked_custom_fields.tipologia = type_custom_fields.id where (type_custom_fields.figlia_di in (select id from type_custom_fields where (type_custom_fields.figlia_di IS NULL) AND (type_custom_fields.riferimento ='issue')) AND (type_custom_fields.riferimento ='issue') )").collect{ |t| [t.valore, t.id]} ,:include_blank => true
         html += "</p>"
         
         html += "<p>"
@@ -34,6 +34,17 @@ class LinkedFieldsHooks < Redmine::Hook::ViewListener
         html
    end    
     end
+
+
+
+
+#def check_for_linked
+#   if @issue.project.module_enabled?(:linked_custom_fields)
+#     if  params[:linked_custom_id1].blank?
+#       render_error (":linked_custom_id1")
+#     end
+#   end
+#end
 
 
 #def view_issues_form_details_top(params)
@@ -67,10 +78,43 @@ class LinkedFieldsHooks < Redmine::Hook::ViewListener
         end
     end
 end
+
  def protect_against_forgery?
     false
-   end
+  end
+  
+  
+  
+  def view_issues_show_description_bottom(params)
+   
+       @issue = params[:issue]
+      if @issue.project.module_enabled?(:linked_custom_fields)
+        @area = LinkedCustomFields.find_by_id(@issue.linked_custom_id1)
+        if (@area)
+          s="<tr><td><b>Area:</b></td><td>#{@area.valore}</td></tr>"
+          @applicativo =LinkedCustomFields.find_by_id(@issue.linked_custom_id2)
+          if (@applicativo)
+            s+="<tr><td><b>Applicativo:</b></td><td>#{@applicativo.valore}</td></tr>"
+            @modulo =LinkedCustomFields.find_by_id(@issue.linked_custom_id3)
+           if (@modulo)
+            s+="<tr><td><b>Modulo:</b></td><td>#{@modulo.valore}</td></tr>"
+           end
+          end
+         return s
+        else
+         return ""
+     end
+   else
+     return ""
+    end
 end
+
+  
+  
+  
+end
+
+
 
 
 #    if authorize_for('linked_custom_fields', 'create')
